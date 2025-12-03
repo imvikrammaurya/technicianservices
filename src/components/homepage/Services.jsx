c26672d440660facd2e13c6b84c890f67d7b280b
Vikram Maurya <imvikrammaurya@outlook.com>
2025-12-03 07:06:50 +0530

    Woking on other components of home section

--- src/components/homepage/Services.jsx
+++ src/components/homepage/Services.jsx
@@ -1,7 +1,51 @@
+import { SeasonalHoverCards } from "@/components/lightswind/seasonal-hover-cards";
+import ac1 from "../../assets/Images/ac_service_1.png";
+import ac2 from "../../assets/Images/ac_service_2.png";
+import ac3 from "../../assets/Images/ac_service_3.png";
+import wm1 from "../../assets/Images/wm_service_1.png";
+import wm2 from "../../assets/Images/wm_service_2.png";
+import wm3 from "../../assets/Images/wm_service_3.png";
+import pf1 from "../../assets/Images/pf_service_1.png";
+import pf2 from "../../assets/Images/pf_service_2.png";
+import pf3 from "../../assets/Images/pf_service_3.png";
+
 export default function Services() {
+  const serviceCards = [
+    {
+      title: "Air conditioner repair and maintenance",
+      subtitle: "Cooling Solutions",
+      description:
+        "We fix broken units, clean filters, and keep systems running efficiently all year.",
+      // PASS 3 IMAGES HERE
+      imageSrcs: [ac1, ac2, ac3],
+    },
+    {
+      title: "Washing machine installation and repair",
+      subtitle: "Laundary Solutions",
+      description:
+        "From new installations to fixing leaks and mechanical issues, we handle it all.",
+      imageSrcs: [wm1, wm2, wm3],
+    },
+    {
+      title: "Purifier repair and installation services",
+      subtitle: "Purification Solutions",
+      description:
+        "Clean air starts with proper maintenance. We install and service all major purifier models.",
+      imageSrcs: [pf1, pf2, pf3],
+    },
+  ];
   return (
     <>
-      <h1>Services</h1>
+      <div>
+        <div className="flex flex-col items-center mt-35">
+          <p className="font-extrabold text-3xl">Services</p>
+          <h4>What we repair and install</h4>
+          <p>Fast, reliable work on the equipment that matters most to you.</p>
+        </div>
+        <div className="my-5  mx-auto px-32">
+          <SeasonalHoverCards cards={serviceCards} />
+        </div>
+      </div>
     </>
   );
 }
